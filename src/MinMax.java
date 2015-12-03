f1e482184f86d62ca11f286565a1a4db91ba6c29
unknown <g00289968@ENG506.gmadtree.gmit.ie>
2015-12-03 13:23:59 +0000

    Initial commit

---
+++ src/MinMax.java
@@ -0,0 +1,73 @@
+import org.json.JSONObject;
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+
+@WebServlet("/MinMax")
+public class MinMax extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String sts = "";
+        String inputString = request.getParameter("num");
+        try {
+
+
+            System.out.println("String of creation is: "+inputString );
+            System.out.println("\nBefore creation of JSON");
+            JSONObject inputValues = new JSONObject(inputString);
+            System.out.println("I have created JSON object");
+
+
+            double[] array = new double[5];
+
+             array[0]= inputValues.getDouble("one");
+             array[1] = inputValues.getDouble("two");
+             array[2]= inputValues.getDouble("three");
+             array[3]= inputValues.getDouble("four");
+             array[4]= inputValues.getDouble("five");
+            //***********************************************************//
+
+            System.out.println(array[0]);
+            System.out.println(array[1]);
+            System.out.println(array[2]);
+            System.out.println(array[3]);
+            System.out.println(array[4]);
+            //***********************************************************//
+
+            Arrays.sort(array);
+
+            double max = array[0];
+            double min = array[array.length-1];
+            double sum = 0;
+            for (double i : array)
+              sum += i;
+
+
+            JSONObject jsn = new JSONObject();
+            jsn.put("max",max);
+            jsn.put("min",min);
+            jsn.put("sum",sum);
+
+            PrintWriter out = response.getWriter();
+            out.println(jsn);
+
+
+        } catch (Exception e) {
+            System.out.println("Servlet died");
+            e.printStackTrace();
+        }
+        PrintWriter out = response.getWriter();
+        out.println(sts);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
